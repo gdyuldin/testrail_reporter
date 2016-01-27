1f12e783d6056b27981d77a2024a1054d3ad69b4
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-01-27 19:27:15 +0300

    Fix: corrected matching of tempest uuid

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,13 +4,21 @@ from testrail_reporter.utils import get_testcase_id
 
 
 @pytest.mark.parametrize('case_name, expected_id', (
-    ('test_ban_l3_agent[once](12345)', '12345'),
-    ('test_ban_l3_agent(12)[once](12345)', '12345'),
-    ('(12345)test_ban_l3_agent[once]', '12345'),
-    ('test_ban_l3_agent(123)', None),
+    ('test_ban_l3_agent[once][(12345)]', '12345'),
+    ('test_ban_l3_agent_54321[once][(12345)]', '12345'),
+    ('12345_test_ban_l3_agent[once]', None),
+    ('test_ban_l3_agent_123', None),
     ('test_ban_l3_agent_12345', None),
     (
         'test_quotas[id-2390f766-836d-40ef-9aeb-e810d78207fb]',
+        '2390f766-836d-40ef-9aeb-e810d78207fb'
+    ),
+    (
+        'test_quotas[id-2390f766-836d-40ef-9aeb-e810d78207fb,network]',
+        '2390f766-836d-40ef-9aeb-e810d78207fb'
+    ),
+    (
+        'test_quotas[network,id-2390f766-836d-40ef-9aeb-e810d78207fb,network]',
         '2390f766-836d-40ef-9aeb-e810d78207fb'
     ),
     (
